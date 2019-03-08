e5c9f40e406515269ba64f05b6550e9a13282978
Eric Laursen <ericjlaursen@gmail.com>
2019-03-07 20:46:06 -0500

    creates first async version

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const fetch = require("node-fetch");
 
-const getGHUser = (userName) => {
+//'VANILLA' FETCH START
+// const getGHUser = (userName) => {
+// 	const url = `https://api.github.com/users/${userName}`
+// 	fetch(url).then(res => res.json())
+// 		.then(resData => {
+// 			console.log('resData')
+// 			console.log(resData)
+// 		})
+// }
+
+
+//1. async
+async function getGHUser(userName){
 	const url = `https://api.github.com/users/${userName}`
-	fetch(url).then(res => res.json())
-		.then(resData => {
-			console.log('resData')
-			console.log(resData)
-			
-		})
+	
+/*
+	2. Await
+		wait till fetch completes
+		takes a promise
+		pauses fn execution till promise is settled
+		IF rejected, throws rejected val
+		IF resolved, returns resolve
+		can assign to var, myRes
+
+		plus await keyword
+*/
+	const myRes = await fetch(url)
+	const userData = await myRes.json()
+
+	console.log('userData')
+	console.log(userData)
 }
 
 getGHUser('imagineLife')
