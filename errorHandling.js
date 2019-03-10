017ab96651f08e6140c350f617cd18f941fb2768
Eric Laursen <ericjlaursen@gmail.com>
2019-03-09 21:15:02 -0500

    starts a look into error handling

---
+++ errorHandling.js
@@ -0,0 +1,29 @@
+/*
+	Part 2
+	error handling
+	wshat if user doesn't exist?!
+*/
+
+const fetch = require("node-fetch");
+
+async function getGHUser(userName){
+	const url = `https://api.github.com/users/${userName}`
+	const myRes = await fetch(url)
+	
+	//WAS return await myRes.json
+	//1. store res in var
+	const resBody = await myRes.json()
+
+	//2 make IF around myRes 
+	if(myRes.status !== 200){
+		//Throw error, passes to promis optional catch block
+		throw Error(resBody.message)
+	}
+
+	
+}
+
+getGHUser('imagineLifeasdf').then(res => {
+	console.log('res')
+	console.log(res)
+})
