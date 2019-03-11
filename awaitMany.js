70c26e9c1a1c3730c799a93695ac2a3a06a7fdc0
Eric Laursen <ericjlaursen@gmail.com>
2019-03-10 22:03:49 -0400

    awaits 2 promises using promise.all

--- awaitMany.js
+++ awaitMany.js
@@ -5,7 +5,7 @@
 
 const fetch = require("node-fetch");
 
-async function getGHUser(ghEndpoint){
+async function getGHData(ghEndpoint){
 	const url = `https://api.github.com${ghEndpoint}`
 	const userResponse = await fetch(url)
 	return await userResponse.json();
@@ -15,6 +15,24 @@ async function getGHUser(ghEndpoint){
 
 async function showUserAndProfile(userName){
 	
+	//promise.all aggregates many promises,
+	//and returns a single promise
+	//if both promises work, a single promise is returned
+	//if one or both promise fails,
+	//a failed promise is returned
+
+	//STORING the promise.all result in a var
+	const [user, repos] = await Promise.all([
+
+		//takes an array of promises
+		getGHData(`/users/${userName}`),
+		getGHData(`/users/${userName}/repos`)
+	])
+
+	// console.log('user.name')
+	// console.log(user.name)
+	// console.log('repos')
+	// console.log(repos.length)
 }
 
 showUserAndProfile('imagineLife')
